98454fce1b92836f0c3953a98fe20cc9a7ec9947
Ghanshyam Bhutra <mail.gbhutra@gmail.com>
2016-11-16 19:14:39 -0600

    storyEngine

---
+++ Assets/Scripts/MotherBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherBehaviourScript : MonoBehaviour {
+
+	private float timeOut = 5.0f;
+
+	bool b_Talking = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (b_Talking) {
+			print ("Mother: talking"); 
+			timeOut = timeOut - Time.deltaTime;
+		}
+		else
+			print ("Mother: NOT talking"); 
+
+		if (0 > timeOut) {
+			timeOut = 2.0f;
+			b_Talking = false;
+		}
+	}
+
+	public void StartTalking() {
+		b_Talking = true;
+	}
+
+	public bool isTalking()	{
+		return b_Talking;
+	}
+}
