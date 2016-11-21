2f70aab2786a94ea726b467704e90fd598454b75
Mohamed Suhail <mohamedsuhail.1990@gmail.com>
2016-11-20 18:13:01 -0600

    Player tagging and fixing weirdness with multiplayer
    Fixed weird flying glitch, and now tag players.

---
+++ Assets/Scripts/TagPlayers.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TagPlayers : MonoBehaviour
+{
+
+    private bool foundFirstPlayer = false;
+    private bool foundSecondPlayer = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!foundFirstPlayer)
+        {
+
+            if (GameObject.FindGameObjectWithTag("Player") != null)
+            {
+                GameObject firstPlayer = GameObject.FindGameObjectWithTag("Player");
+                firstPlayer.gameObject.tag = "FirstPlayer";
+                foundFirstPlayer = true;
+            }
+        }
+
+        if (!foundSecondPlayer && foundFirstPlayer)
+        {
+            if (GameObject.FindGameObjectWithTag("Player") != null)
+            {
+                GameObject secondPlayer = GameObject.FindGameObjectWithTag("Player");
+                secondPlayer.gameObject.tag = "SecondPlayer";
+                foundSecondPlayer = true;
+            }
+
+        }
+    }
+}
