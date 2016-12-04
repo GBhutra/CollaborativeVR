9c2007bb9cd9db6f69458858234a4ff5810dc3d1
Cullen Brown <cjb3768@gmail.com>
2016-12-04 14:22:16 -0600

    Triggering an animation on keypress
    Got it working for one animation. Writing functions to make it easier to
    call animation switches.

---
+++ Assets/Animations/Mother/MotherAnimationHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherAnimationHandler : MonoBehaviour
+{
+    Animator anim;
+
+    int idleToRun = Animator.StringToHash("Idle-Running");
+    int runToIdle = Animator.StringToHash("Running-Idle");
+    int idleStateHash = Animator.StringToHash("Base Layer.Idle");
+    int runningStateHash = Animator.StringToHash("Base Layer.Running");
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        //assign arbitrary button to call animations
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (stateInfo.fullPathHash == runningStateHash)
+            {
+                anim.SetTrigger(runToIdle);
+            }
+            else if (stateInfo.fullPathHash == idleStateHash)
+            {
+                anim.SetTrigger(idleToRun);
+            }
+        }
+    }
+}
